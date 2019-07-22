dd4da2836dd75b4623751250e53b2d0b38f4dcab
GIS <you@example.com>
2019-07-22 13:58:42 +0700

    Initial Commit

---
+++ IOS-UISwitchTests/IOS_UISwitchTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_UISwitchTests.swift
+//  IOS-UISwitchTests
+//
+//  Created by GIS on 7/22/19.
+//  Copyright © 2019 GIS. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_UISwitch
+
+class IOS_UISwitchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
